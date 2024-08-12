d50526fae89f1a8d19f49979bf55931a319dbb79
Alan Benavides <54295964+Kykal@users.noreply.github.com>
2024-08-11 20:04:30 -0600

    x

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+//Main component content
+const App = (): JSX.Element => {
+  //Main component render
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
+};
+
+
+export default App; //Export main component
