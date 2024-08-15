1d2e855167c70bc6c21bbbb2f59d69932a559390
Alan Benavides <54295964+Kykal@users.noreply.github.com>
2024-08-15 17:49:24 -0600

    First commit

---
+++ app/layout.tsx
@@ -0,0 +1,47 @@
+//MATERIAL DESIGN
+//Components
+import { ColorSchemeScript, MantineProvider } from '@mantine/core';
+
+
+//Styles
+import "./globals.css";
+import '@mantine/core/styles.css';
+import { Montserrat } from "next/font/google";
+
+
+const monsterrat = Montserrat({
+  subsets: ['latin']
+});
+
+
+export const metadata: Metadata = {
+  title: 'Prueba de desarrollo - Admira',
+};
+
+
+//Types
+import type { Metadata } from "next";
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+
+//Main component content
+const RootLayout = ({children}: LayoutProps): JSX.Element => {
+  //Main component render
+  return (
+    <html lang='en' >
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body className={monsterrat.className}>
+        <MantineProvider>
+          {children}
+        </MantineProvider>
+      </body>
+    </html>
+  );
+};
+
+
+export default RootLayout; //Export main component
