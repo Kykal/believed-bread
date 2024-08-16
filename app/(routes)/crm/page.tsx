bb53ad846bd0960e905d9bda8cf2d97423dbb925
Alan Benavides <54295964+Kykal@users.noreply.github.com>
2024-08-16 01:49:30 -0600

    CRM
    Additions

--- app/(routes)/crm/page.tsx
+++ app/(routes)/crm/page.tsx
@@ -1,9 +1,13 @@
+//Components
+import CrmHeader from './_components/CrmHeader';
+
+
 //Main component content
 const Page = (): JSX.Element => {
 	//Main component render
 	return (
 		<>
-			CRM
+			<CrmHeader />
 		</>
 	);
 };
