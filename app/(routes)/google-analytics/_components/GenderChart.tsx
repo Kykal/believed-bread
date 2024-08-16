f7617ed7d64b541536f479c6ef4b75e6965171ef
Alan Benavides <54295964+Kykal@users.noreply.github.com>
2024-08-16 01:16:50 -0600

    Create GenderChart.tsx

---
+++ app/(routes)/google-analytics/_components/GenderChart.tsx
@@ -0,0 +1,24 @@
+//Components
+import PrefabCard from '../../_components/PrefabCard';
+
+
+//Types
+type GenderChartProps = {
+	
+};
+
+
+//Main component content
+const GenderChart = (props: GenderChartProps): JSX.Element => {
+
+
+	//Main component render
+	return (
+		<PrefabCard title='Género' >
+			a
+		</PrefabCard>
+	);
+};
+
+
+export default GenderChart; //Export main component
